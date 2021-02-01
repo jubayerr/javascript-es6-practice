4b61a62cc62cbe4e6d217c2e3ce335370f3357ec
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-02-01 17:06:39 +0600

    more code added

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const greeting = "Hello World!";
+// default parameter
 
-console.log(greeting);
+// function add(num1, num2 = 0) {
+//     return num1 + num2;
+// }
+
+// const total = add(15, 20);
+
+// console.log(total);
+
+// tamplate parameter
+
+const firstName = "Justin";
+const lastName = "TimberLake";
+
+const fullName = `${firstName} ${lastName} is a good boy`;
+console.log(fullName);
